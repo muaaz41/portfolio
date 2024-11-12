f4b797f2b872f20420b4259b48c672d1b41e9ec4
muaaz41 <munirmuaaz@gmail.com>
2024-11-12 17:26:56 +0500

    irjfn

--- experience/script.js
+++ experience/script.js
@@ -69,7 +69,7 @@ function(){
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
+        document.title = "Portfolio | Muaaz Bhatti";
+        $("#favicon").attr("href","/assets/images/favicon.png");
     }
 });
